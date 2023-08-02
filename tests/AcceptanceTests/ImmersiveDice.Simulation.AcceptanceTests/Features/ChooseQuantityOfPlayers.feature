d747c29c9dac17c1286e0954f18a1e832a3dd512
MaximCote <maximcotedeveloper@duck.com>
2023-08-01 20:40:57 -0400

    Adding acceptance test for the feature enabling the user to choose the quantity of players in the simulation.

---
+++ tests/AcceptanceTests/ImmersiveDice.Simulation.AcceptanceTests/Features/ChooseQuantityOfPlayers.feature
@@ -0,0 +1,23 @@
+﻿Feature: Choose Quantity Of Players
+
+As a
+Game designer
+I want to
+choose the quantity of players for a play-through simulation
+So that I can
+evaluate the effect on simulated data when I input a different quantity of players
+
+Background:
+Given a new simulation is being created
+
+Scenario: User choose the quantity of players for the simulation
+Given the number for the quantity of players is 2
+And 2 is within the allowed range by the game rules
+When the simulation creation is confirmed
+Then the quantity of players in the simulation configuration should be 2
+
+#Scenario Outline: User choose the quantity of players for the simulation within the allowed range
+#Given the number for the quantity of players is <QuantityOfPlayers>
+#And <QuantityOfPlayers> is within the allowed <Range> by the game rules
+#When the simulation creation is confirmed
+#Then the quantity of players in the simulation configuration should be <Result>
---
+++ tests/AcceptanceTests/ImmersiveDice.Simulation.AcceptanceTests/StepDefinitions/ChooseQuantityOfPlayersStepDefinitions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImmersiveDice.Simulation.AcceptanceTests.StepDefinitions
+{
+    [Binding]
+    public sealed class ChooseQuantityOfPlayersStepDefinitions
+    {
+        [Given(@"a new simulation is being created")]
+        public void GivenANewSimulationIsBeingCreated()
+        {
+            //TODO: implement arrange (precondition) logic
+
+            throw new PendingStepException();
+        }
+
+        [Given("the number for the quantity of players is (.*)")]
+        public void GivenTheQuantityOfPlayersIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+
+            throw new PendingStepException();
+        }
+
+        [Given("(.*) is within (.*)")]
+        public void GivenTheQuantityOfPlayersIsWithin(int number, Range range)
+        {
+            //TODO: implement arrange (precondition) logic
+
+            throw new PendingStepException();
+        }
+
+        [When("the simulation creation is confirmed")]
+        public void WhenTheSimulationCreationIsConfirmed()
+        {
+            //TODO: implement act (action) logic
+
+            throw new PendingStepException();
+        }
+
+        [Then("the quantity of players in the simulation configuration should be (.*)")]
+        public void ThenTheQuantityOfPlayersInTheConfigurationShouldBe(int result)
+        {
+            //TODO: implement assert (verification) logic
+
+            throw new PendingStepException();
+        }        
+    }
+}
